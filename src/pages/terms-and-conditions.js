a9da66a4fbebdf662c27015c5760f678e8f9c13f
Daniel Balloch <danielpbballoch@gmail.com>
2023-12-30 20:09:51 +1300

    support and terms pages responsive

--- src/pages/terms-and-conditions.js
+++ src/pages/terms-and-conditions.js
@@ -1,16 +1,35 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { graphql } from "gatsby"
+import { StructuredText } from 'react-datocms';
+import styled from "@emotion/styled"
+
+const Wrapper = styled.div`
+max-width: 900px;
+margin: auto;
+padding: 200px 20px 100px 20px;
+`
 
 
 
-
-const IndexPage = () => {
+const IndexPage = (data) => {
+  console.log(data)
   return (
     <Layout invert={true}>
-      <div>
-        <h1>Terms & Conditions</h1>
-      </div>
+      <Wrapper>
+        <StructuredText
+            data={data.data.datoCmsTermsAndConditionsPage.termsAndConditions.value}
+            renderInlineRecord={({ record }) => {
+                switch (record.__typename) {
+                case 'DatoCmsArticle':
+                    return <a href={`/articles/${record.slug}`}>{record.title}</a>;
+                default:
+                    return null;
+                }
+            }}
+        />
+      </Wrapper>
     </Layout>
   )
 }
@@ -21,3 +40,13 @@ export const Head = () => <SEO
 title="MotoSchool | Terms & Conditions"
 description="View Motoschools Terms & Conditions"
 />
+
+export const pageQuery = graphql`
+    query TermsAndConditions{
+      datoCmsTermsAndConditionsPage {
+        termsAndConditions {
+          value
+        }
+    }
+    }
+`
